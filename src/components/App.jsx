908013d7d8a65170d03cc930b795aec76d17fb66
Mash_xf <felix.macharia@student.moringaschool.com>
2026-05-06 19:18:37 +0300

    correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import blogData from './blog.js'
-import Header from './component/header.jsx'
-import About from './component/about.jsx'
-import ArticleList from './component/articlelist.jsx'
+import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 function App() {
-
   return (
-    <>
+    <div className="App">
       <Header name={blogData.name} />
       <About image={blogData.image} about={blogData.about} />
       <ArticleList posts={blogData.posts} />
-    </>
-  )
+    </div>
+  );
 }
-
 
 export default App;
